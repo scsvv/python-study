b580aba4b0c6737c332dca092f7f4ccf59375281
Zvat Scorobogatov <zvat.333333@gmail.com>
2021-07-21 12:03:58 +0300

    разбиение заданий в  разные файлы

--- hm-2/main.py
+++ hm-2/main.py
@@ -1,42 +1,7 @@
-ProgramName = '**** ***   *** *****\n*    *  * *  *   *\n*    ***   ***   *\n*    *  *  * *   *\n**** ***  *  *   *\n'
-print("task 5: \n\n" + ProgramName)
+import task5
+import task6
+import task7
 
-'''
-output of line 2:  
-
-**** ***   *** *****
-*    *  * *  *   *
-*    ***   ***   *
-*    *  *  * *   *
-**** ***  *  *   *
-'''
-
-print(
-    '''
-    task 6: 
-    \\a - Bell(allert)
-    \\b - Backspace
-    \\n - New line
-    \\t - Horizontal tab
-    \\\ - Backslash \\
-    \\" - Double quotation mark "
-    \\' - Single quotation mark '
-    '''
-)
-
-
-
-print("task 7")
-name = input("Привет меня зовут Жарвис а тебя как зовут : ")
-input(f'Как дела {name}?: ') #не создаю переменную под это выражение, так как информация, не используется и не стоит засорять память этой инфой (моё мнение)
-
-x = input("Загадай число первое число: ")
-y = input("Загадай число второе число: ")
-
-try: 
-    z = int(x) + int(y)
-except ValueError: 
-    z = 0
-
-print(f'Сумма чисел равна {z}')
-
+task5
+task6
+task7
---
+++ hm-2/task6.py
@@ -0,0 +1,12 @@
+print(
+    '''
+    task 6: 
+    \\a - Bell(allert)
+    \\b - Backspace
+    \\n - New line
+    \\t - Horizontal tab
+    \\\ - Backslash \\
+    \\" - Double quotation mark "
+    \\' - Single quotation mark '
+    '''
+)
