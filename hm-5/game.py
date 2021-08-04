a59331f85c5e6d6c38ec976b607efe9f62c90331
Zvat Scorobogatov <zvat.333333@gmail.com>
2021-08-04 21:47:29 +0300

    count

--- hm-5/game.py
+++ hm-5/game.py
@@ -1,17 +1,18 @@
 from random import randint 
 
 machine_mumber = randint(1, 100)
+count = 1
 
 while True:
     user_mumber = input("Отгадайте число от 1 до 100: ")
-
+    count += 1
     try: 
         user_mumber = int(user_mumber)
     except ValueError:
         user_mumber = -100
     
     if user_mumber == machine_mumber:
-        print(f'Вы угадали моё число')
+        print(f'Вы угадали моё число\nКоличество попыток: {count}')
         break
     elif user_mumber == -100:
         print(f'Введите число!!!!!')
@@ -19,6 +20,8 @@ while True:
         print(f'Моё число меньше')
     elif user_mumber < machine_mumber: 
         print(f'Моё число больше')
+    
+
 
 
 
