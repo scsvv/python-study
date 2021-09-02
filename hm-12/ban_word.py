452ed294c336e131828ac7f2079315628e30d921
Скоробогатов Святослав <45883075+CarbonSkorobogatov@users.noreply.github.com>
2021-09-02 10:37:54 +0300

    PEP8

--- hm-12/ban_word.py
+++ hm-12/ban_word.py
@@ -1,13 +1,13 @@
 class BanWarningWord(Exception):
-    '''
+    """
     Exception, raised when banned word used.
     It has optional message attribute for the
     convenient problem describing.
-    '''
-    message : str = 'Please, don\'t use banned word'
+    """
+    message: str = 'Please, don\'t use banned word'
 
-    def __init__(self, message = None): 
-        if message != None:
+    def __init__(self, message=None):
+        if message:
             self.message = message
 
     def __str__(self):
@@ -17,7 +17,7 @@ class BanWarningWord(Exception):
 if __name__ == "__main__":
     try:
         baned_word = "cat"
-        sentance : str = input("Enter your sentance: \n")
+        sentance: str = input("Enter your sentance: \n")
         sentance = sentance.split()
         
         if sentance.count(baned_word) != 0:
