4893c5536ed5d8ac341b2d274c09197be53c8d6e
Скоробогатов Святослав <45883075+CarbonSkorobogatov@users.noreply.github.com>
2021-09-02 10:38:38 +0300

    PEP8

--- hm-12/try_find_sum.py
+++ hm-12/try_find_sum.py
@@ -1,9 +1,9 @@
-def find_sum_of_number(a : str, b : str):
-
-    try: 
+def find_sum_of_number(a: str, b: str):
+    try:
         return float(a) + float(b)
-    except ValueError: 
+    except ValueError:
         return str(a) + str(b)
+
 
 if __name__ == "__main__":
     a = input("Enter first number: ")
